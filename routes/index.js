d80753abe6b9a41241bc1021f8d2fad843057c5b
Marina Sorokina <Sorokina2525@yandex.ru>
2023-08-26 20:34:19 +0400

    Перенесена ошибка 404 в index.js

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const movieRouter = require('./movies');
 const authRouter = require('./authorization');
 const regRouter = require('./registration');
 const { auth } = require('../middlewares/auth');
+const NotFound = require('../errors/NotFound');
 
 router.use('/', regRouter);
 router.use('/', authRouter);
 router.use('/', auth, userRouter);
 router.use('/', auth, movieRouter);
+router.use('*', (_req, _res, next) => next(new NotFound('Страница не найдена')));
 
 module.exports = router;
