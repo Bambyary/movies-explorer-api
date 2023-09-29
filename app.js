0cbfc41a02d92e669992914d831ce5df7b28ab71
Marina Sorokina <Sorokina2525@yandex.ru>
2023-09-29 17:53:50 +0400

    переделала cors

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 const { limiter } = require('./utils/rateLimiter');
 const { errorHeandler } = require('./middlewares/errorHeandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes/index');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
 const { PORT, MONGO_DB } = require('./models/config');
 
 const app = express();
@@ -21,7 +22,8 @@ mongoose.connect(MONGO_DB)
 
 app.use(helmet());
 app.use(limiter);
-app.use(cors);
+app.use(cors({ origin: ['http://sorokina-diplom.nomoredomainsicu.ru', 'https://sorokina-diplom.nomoredomainsicu.ru', 'http://localhost:3000', 'https://localhost:3000', 'http://localhost:3001', 'https://localhost:3001'] }));
+// app.use(cors);
 app.use(express.json());
 app.use(requestLogger);
 app.use('/', routes);
